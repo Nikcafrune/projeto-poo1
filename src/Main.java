9a34574688582222d2a3d6482a003c54623700dd
Nikcafrune <nikcafrune1@gmail.com>
2025-04-10 14:11:58 -0300

    commit-vogais

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
-public class Main {
+public class ContadorVogais {
 
+    public static int contarVogais(String texto) {
+        int contador = 0;
+        String vogais = "aeiouAEIOU";
 
+        for (char c : texto.toCharArray()) {
+            if (vogais.indexOf(c) != -1) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
 
     public static void main(String[] args) {
-
-
-        System.out.println("Boa tarde Everton.");
-        System.out.println("Recebendo dois números inteiros e retornando soma");
-        System.out.println("Recebe um número inteiro e retorna true se for par e false se for ímpar.");
-        System.out.println("Recebe um número inteiro positivo e retorna seu fatorial.");
-        System.out.println("Recebe uma string e retorna a string invertida.");
-        System.out.println("Recebe uma lista de números e retorna a média aritmética.");
-        System.out.println("Recebe uma string e retorna o número de vogais contidas nela.");
-
-
-        System.out.println(" Valeu demais, tamo junto! ");
-
-
+        String exemplo = "ChatGPT é incrível!";
+        int totalVogais = contarVogais(exemplo);
+        System.out.println("Número de vogais: " + totalVogais);
     }
 }
