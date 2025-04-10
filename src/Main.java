21803a7e021a3d238c8399484dbef40c39d38737
Nikcafrune <nikcafrune1@gmail.com>
2025-04-10 13:40:15 -0300

    commit-soma

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
 public class Main {
 
 
+        public static int somar(int a, int b) {
+            return a + b;
+        }
 
-    public static void main(String[] args) {
-
-
-        System.out.println("Boa tarde Everton.");
-        System.out.println("Recebendo dois números inteiros e retornando soma");
-        System.out.println("Recebe um número inteiro e retorna true se for par e false se for ímpar.");
-        System.out.println("Recebe um número inteiro positivo e retorna seu fatorial.");
-        System.out.println("Recebe uma string e retorna a string invertida.");
-        System.out.println("Recebe uma lista de números e retorna a média aritmética.");
-        System.out.println("Recebe uma string e retorna o número de vogais contidas nela.");
-
-
-        System.out.println(" Valeu demais, tamo junto! ");
-
-
+        public static void main(String[] args) {
+            int resultado = somar(5, 7);
+            System.out.println("A soma é: " + resultado);
+        }
     }
-}
+
+
+
