2c2367210e929c4612be3feb3fce26a3d6aad20b
Nikcafrune <nikcafrune1@gmail.com>
2025-04-10 14:08:39 -0300

    commit-invertida

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
-public class Main {
+public class InverterString {
 
-
+    public static String inverter(String texto) {
+        return new StringBuilder(texto).reverse().toString();
+    }
 
     public static void main(String[] args) {
-
-
-        System.out.println("Boa tarde Everton.");
-        System.out.println("Recebendo dois números inteiros e retornando soma");
-        System.out.println("Recebe um número inteiro e retorna true se for par e false se for ímpar.");
-        System.out.println("Recebe um número inteiro positivo e retorna seu fatorial.");
-        System.out.println("Recebe uma string e retorna a string invertida.");
-        System.out.println("Recebe uma lista de números e retorna a média aritmética.");
-        System.out.println("Recebe uma string e retorna o número de vogais contidas nela.");
-
-
-        System.out.println(" Valeu demais, tamo junto! ");
-
-
+        String original = "hello world";
+        String invertida = inverter(original);
+        System.out.println("String original: " + original);
+        System.out.println("String invertida: " + invertida);
     }
 }
